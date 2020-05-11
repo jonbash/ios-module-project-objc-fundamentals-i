79bdcb7150deb3868c2470a6a3414c6c2e69735d
Jon Bash <jonbash@protonmail.com>
2020-05-11 14:31:05 -0700

    start xcode proj

---
+++ ObjC_NOCList/ObjC_NOCList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC_NOCList
+//
+//  Created by Jon Bash on 2020-05-11.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
